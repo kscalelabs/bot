18a0aadb0c917accced286b121d19a325d7daff5
Benjamin Bolte <ben@bolte.cc>
2023-10-09 04:29:46 -0400

    spinner

--- frontend/src/pages/Dashboard/SettingsPage/components/DeleteAccountComponent.tsx
+++ frontend/src/pages/Dashboard/SettingsPage/components/DeleteAccountComponent.tsx
@@ -1,27 +1,31 @@
 import { humanReadableError, useApi } from "constants/backend";
 import { useToken } from "hooks/auth";
 import { useState } from "react";
-import { Button, Form } from "react-bootstrap";
+import { Button, Form, Spinner } from "react-bootstrap";
 
 const DeleteAccountComponent = () => {
   const [buttonEnabled, setButtonEnabled] = useState(false);
+  const [useSpinner, setUseSpinner] = useState(false);
 
   const api = useApi();
   const { setToken } = useToken();
 
   const handleDeleteAccount = async (e: React.FormEvent) => {
     e.preventDefault();
+    setUseSpinner(true);
     try {
       await api.delete<boolean>("/users/myself");
       setToken(null);
     } catch (error) {
       console.log(humanReadableError(error));
       setButtonEnabled(false);
+    } finally {
+      setUseSpinner(false);
     }
   };
 
   return (
-    <Form.Group className="mt-3">
+    <Form.Group>
       <Form.Check
         type="switch"
         label="Confirm account deletion"
@@ -29,17 +33,21 @@ const DeleteAccountComponent = () => {
           setButtonEnabled(e.target.checked);
         }}
         checked={buttonEnabled}
+        className="mb-3"
       />
 
-      <Button
-        variant="danger"
-        id="button-addon2"
-        className="mt-3"
-        disabled={!buttonEnabled}
-        onClick={handleDeleteAccount}
-      >
-        <i className="fa fa-cross"></i> Delete Account
-      </Button>
+      {useSpinner ? (
+        <Spinner />
+      ) : (
+        <Button
+          variant="danger"
+          id="button-addon2"
+          disabled={!buttonEnabled}
+          onClick={handleDeleteAccount}
+        >
+          <i className="fa fa-cross"></i> Delete Account
+        </Button>
+      )}
     </Form.Group>
   );
 };
