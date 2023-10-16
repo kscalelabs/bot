d62a34fb1f8c00b97bc23aa9c0dfb0dff4d79b8e
Benjamin Bolte <ben@bolte.cc>
2023-10-16 19:18:51 -0400

    improved backend build flow

---
+++ scripts/build_docker_images.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+# Builds the Docker image for the AWS Lambda deployment.
+# Usage:
+#   ./scripts/build_docker_images.sh
+
+set -e
+
+home_dir=$(pwd)
+
+# Checks that the home directory is the root of the project.
+if [[ ! -f "bot/requirements.txt" ]]; then
+  echo "Error: Please run this script from the root of the project."
+  exit 1
+fi
+
+# Checks CLI arguments.
+if [[ $# -ne 0 ]]; then
+  echo "Error: Invalid number of arguments."
+  echo "Usage: ./scripts/build_docker_images.sh"
+  exit 1
+fi
+
+# Creates the dist directory if it doesn't exist.
+dist_dir=dist/
+mkdir -p $dist_dir
+
+# Gets a unique JWT secret.
+export JWT_SECRET=$(python -c "import secrets; print(secrets.token_urlsafe(32))")
+
+# Builds the configuration file.
+python configs/build.py aws -o ${dist_dir}/config.yaml
+
+# Downloads a static copy of FFMPEG.
+if [[ ! -f "${dist_dir}/ffmpeg/bin/ffmpeg" ]]; then
+  echo "Downloading static FFMPEG files..."
+  wget https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz
+  wget https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz.md5
+  md5sum -c ffmpeg-release-amd64-static.tar.xz.md5
+  mkdir -p ${dist_dir}/ffmpeg/bin/
+  tar -xf ffmpeg-release-amd64-static.tar.xz -C ${dist_dir}/ffmpeg/bin/ --strip-components=1
+  rm ffmpeg-release-amd64-static.tar.xz ffmpeg-release-amd64-static.tar.xz.md5
+fi
+
+# Log in to ECR.
+aws ecr get-login-password | docker login --username AWS --password-stdin ${ECR_URI}
+
+# Builds the API Docker image.
+docker build -t dpsh-api -f scripts/docker/Dockerfile.api .
+docker tag dpsh-api:latest ${ECR_URI}:latest-api
+docker push ${ECR_URI}:latest-api
+docker rmi dpsh-api:latest
+
+# Builds the worker Docker image.
+docker build -t dpsh-worker -f scripts/docker/Dockerfile.worker .
+docker tag dpsh-worker:latest ${ECR_URI}:latest-worker
+docker push ${ECR_URI}:latest-worker
+docker rmi dpsh-worker:latest
